f6c7ceaed310b24bf5b8dc08409c8b7629e8a1c8
jcarango <jcamonsalve01@gmail.com>
2023-05-27 00:16:26 -0500

    Filter bar fixed

--- src/ListaProductos/Listado.jsx
+++ src/ListaProductos/Listado.jsx
@@ -1,25 +1,38 @@
 import { useState, useEffect } from "react";
 import { Card } from "../Card/Card";
 import Styles from './listado.module.css'
-import { SearchBar } from "../SearchBar/SearchBar";
 
 
 export function ListarProductos() {
     const [products, setProducts] = useState([])
+    const [filteredProducts, setFilteredProducts] = useState([])
+    const [filterSearch, setFilterSearch] = useState('')
     useEffect(()=>{
-        componentDidMount()
+        gettingProducts()
     },[])
-    async function componentDidMount() {
+    async function gettingProducts() {
         // GET request using fetch with async/await
         const response = await fetch('https://api.escuelajs.co/api/v1/products');
         const data = await response.json();
         setProducts(data)
+        setFilteredProducts(data)
+    }
+    function filterResults(evt) {
+        const searchProduct = evt.target.value
+        setFilterSearch(searchProduct)
+        console.log(filterSearch);
+
+        const filterArray = products.filter((productos) => productos.title.includes(searchProduct))
+        setFilteredProducts(filterArray)
     }
     return(
         <>
-        <SearchBar/>
+        <div className={`${Styles.searchBar}`}>
+            <label>Filtrar por palabra clave:</label>
+            <input type="text" value={filterSearch} onChange={filterResults}/>
+        </div>
         <div className={`${Styles.container}`}>
-            {products?.map((product) => (
+            {filteredProducts?.slice(0,20).map((product) => (
                 <Card
                     key={product.id}
                     id={product.id}

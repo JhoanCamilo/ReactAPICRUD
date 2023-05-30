0e18ec2775d7c2e5a31ffca4d4eb55448e603dee
jcarango <jcamonsalve01@gmail.com>
2023-05-30 14:39:06 -0500

    CRUD completed

--- src/SearchBar/SearchBar.jsx
+++
@@ -1,9 +0,0 @@
-import Styles from './SearchBar.module.css'
-export function SearchBar() {
-    return(
-        <div className={`${Styles.searchBar}`}>
-            <label htmlFor="">Filtrar por palabra clave:</label>
-            <input type="text"/>
-        </div>
-    )
-}
